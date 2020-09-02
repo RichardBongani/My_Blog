c1c0cac7df7db80667ab4143060f94b036964f7b
RichardBongani <phalarichard1@gmail.com>
2020-09-02 19:23:05 +0200

    refactoring blog posts by modifying the layout to grid and destroying other nav pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,21 +5,7 @@ class PagesController < ApplicationController
     @time = Time.now
     @title = "Welcome"
   end
-
-  def about
-    names = ['Richard', 'Tintswalo','Bongumusa']
-    @random = names.sample
-    @time = Time.now
-  end
-
-  def services
-    names = ['Richard', 'Tintswalo','Bongumusa']
-    @random = names.sample
-    @time = Time.now
-    @title = "Welcome"
-    @services = ['Web Design', 'Web development', 'Content Marketing']
-  end
-
+  
   def contact
  
   end
